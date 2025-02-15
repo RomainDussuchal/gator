f47aec79e476c287f39bb61f986298ef776ef217
RomainDussuchal <romain-vrb@hotmail.fr>
2025-02-15 17:19:42 +0100

    creating handlers register login reset list

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,12 @@
 package config
 
-const configFileNam =".gatorconfig.json"
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
+const configFileName =".gatorconfig.json"
 
 type Config struct {
   DBURL string `json:"db_url"`
@@ -11,3 +17,52 @@ func (cfg *Config) SetUser(userName string) error {
   cfg.CurrentUserName = userName
   return write(*cfg)
 }
+
+func write(cfg Config) error {
+  fullPath, err := getConfigFilePath()
+  if err != nil {
+    return err
+  }
+  file, err := os.Create(fullPath)
+  if err != nil {
+    return err
+  }
+  defer file.Close()
+  encoder := json.NewEncoder(file)
+  err = encoder.Encode(cfg)
+  if err != nil {
+    return err
+  }
+  return nil
+}
+
+func getConfigFilePath() (string, error) {
+  dir, err := os.UserHomeDir()
+  if err != nil {
+    return "", err
+  }
+  fullPath := filepath.Join(dir, configFileName)
+  return fullPath, nil
+}
+
+func Read() (Config, error) {
+  fullPath, err := getConfigFilePath()
+  if err != nil {
+    return Config{}, err
+  }
+  file, err := os.Open(fullPath)
+  if err != nil {
+    return Config{}, err
+  }
+  defer file.Close()
+  decoder := json.NewDecoder(file)
+   cfg := Config{}
+  err = decoder.Decode(&cfg)
+  if err != nil {
+    return Config{}, err
+  }
+  return cfg, nil
+}
+
+
+
--- main.go
+++ main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/RomainDussuchal/go_projects/gator/internal/config"
+	"github.com/RomainDussuchal/go_projects/gator/internal/database"
+	_ "github.com/lib/pq"
+)
+
+
+type state struct {
+	db  *database.Queries
+	cfg *config.Config
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+		log.Fatalf("error connecting to db: %v", err)
+	}
+	defer db.Close()
+	dbQueries := database.New(db)
+
+	programState := &state{
+		db:  dbQueries,
+		cfg: &cfg,
+	}
+
+	cmds := commands{
+		registeredCommands: make(map[string]func(*state, command) error),
+	}
+	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
+	cmds.register("reset", handlerReset)
+	cmds.register("users", handlerUsers)
+
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: cli <command> [args...]")
+		return
+	}
+
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
+
+	err = cmds.run(programState, command{Name: cmdName, Args: cmdArgs})
+	if err != nil {
+		log.Fatal(err)
+	}
 }
