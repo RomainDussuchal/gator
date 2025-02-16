c0036d432b2f493568999505648b6f085e1cca4e
RomainDussuchal <romain-vrb@hotmail.fr>
2025-02-16 12:31:00 +0100

    gator done

--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module github.com/RomainDussuchal/go_projects/gator
 
 go 1.23.3
 
-require github.com/google/uuid v1.6.0 // indirect
+require github.com/google/uuid v1.6.0 
 
-require github.com/lib/pq v1.10.9 // indirect
+require github.com/lib/pq v1.10.9 
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -13,3 +13,15 @@ WHERE url = $1;
 -- name: DeleteFeed :exec
 DELETE FROM feeds
 WHERE id = $1;
+
+-- name: MarkFeedFetched :one
+UPDATE feeds
+SET   last_fetched_at = NOW(),
+updated_at = NOW()
+WHERE id = $1
+RETURNING *;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
